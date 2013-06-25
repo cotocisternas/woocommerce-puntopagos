0b87372e81a15d95522f94d4ffa91c5da3bc030c
Cristian Tala Sánchez <cristian.tala@gmail.com>
2013-06-24 20:05:02 -0400

    INICIAL

--- webpay.php
+++ webpay.php
@@ -1,42 +1,52 @@
 <?php
+
 /*
   Plugin Name: Woocommerce Webpay ( Chilean Payment Gateway )
   Description: Sistema de pagos de WooCommerce con WebPay
   Author: Cristian Tala Sánchez
   Version: 1.3
   Author URI: www.cristiantala.cl
- 
-This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License or any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License or any later version.
 
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
 
-add_action( 'plugins_loaded', 'init_woocommerce_webpay' );
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
-function add_webpay_gateway_class( $methods ) {
-	$methods[] = 'WC_WebPay'; 
-	return $methods;
+add_action('plugins_loaded', 'init_woocommerce_webpay');
+
+function add_webpay_gateway_class($methods) {
+    $methods[] = 'WC_WebPay';
+    return $methods;
 }
 
-add_filter( 'woocommerce_payment_gateways', 'add_webpay_gateway_class' );
+add_filter('woocommerce_payment_gateways', 'add_webpay_gateway_class');
 
 function init_woocommerce_webpay() {
-	class WC_WebPay extends WC_Payment_Gateway {
+
+    class WC_WebPay extends WC_Payment_Gateway {
+
+        public function __construct() {
+            $this->id = "WebPay GateWay ";
+            $this->method_title = "";
+            $this->method_description = "";
+
+            $this->init_form_fields();
+            $this->init_settings();
             
-            public function __construct() {
-                $this->id = "WebPay GateWay ";
-                $this->method_title = "";
-                $this->method_description = "";
-            }
+            $this->title = $this->get_option( 'title' );
+            add_action( 'woocommerce_update_options_payment_gateways_' . $this->id, array( $this, 'process_admin_options' ) );
         }
+
+    }
+
 }
 
 ?>
